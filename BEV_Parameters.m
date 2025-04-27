a867a367e5af7d5deb60f8e52571b1bfd8a98713
Koushik <koushik96shenoy@gmail.com>
2025-04-27 21:42:26 +0200

    Added parts on individual components of vehicle

--- BEV_Parameters.m
+++ BEV_Parameters.m
@@ -2,4 +2,15 @@ clear all;
 close all;
 clc;
 
-%%
+%% Surroundings
+Angle_of_inclination = 1;
+
+%% Vehicle Parameters
+
+%% DC Motor Parameters
+
+%% Battery Parameters
+
+%% Simulink Model
+
+%% Plotting Performance

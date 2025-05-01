373ead09310469234824be54791a944b7629e27c
Koushik <koushik96shenoy@gmail.com>
2025-05-01 09:42:19 +0200

    Alternate Method of simulation
    Using simscape modelling approach to model motor and motor controller.

--- BEV_Parameters.m
+++ BEV_Parameters.m
@@ -3,9 +3,17 @@ close all;
 clc;
 
 %% Surroundings
-Angle_of_inclination = 1;
+Grade = 0;
+g = 9.81;
+mu_rr = 0.013;
 
 %% Vehicle Parameters
+mass = 1965;
+Cd = 0.29;
+i_ratio = 8;
+Af = 1.550 * 1.770;
+
+r_wheel = 0.016;
 
 %% DC Motor Parameters
 
